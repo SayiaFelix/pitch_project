8ff0c918c5c97ac25a1154a28fea53419de61560
SayiaFelix <sayia.lucas@student.moringaschool.com>
2022-05-07 08:31:43 +0300

    Setting up my models

--- config.py
+++ config.py
@@ -2,9 +2,9 @@ import os
 
 class Config:
 
-    SECRET_KEY=os.environ.get('SECRET_KEY')
+    SECRET_KEY = 'f2GDBACdGn5ZGx$A!gC[:+*/b.JS?('
+    # SECRET_KEY=os.environ.get('SECRET_KEY')
     # SQLALCHEMY_DATABASE_URI ='postgresql+psycopg2://emdee:arif@123@localhost/pitches'
-
 
     UPLOADED_PHOTOS_DEST ='app/static/photos'
 
