360c19702d37be4156c9112794927936d0d3c020
SayiaFelix <sayia.lucas@student.moringaschool.com>
2022-05-07 09:27:39 +0300

    setting my error page and initializing my main

--- manage.py
+++ manage.py
@@ -22,6 +22,7 @@ def test():
     tests = unittest.TestLoader().discover('tests')
     unittest.TextTestRunner(verbosity=2).run(tests)
 
+
 @manager.shell
 def make_shell_context():
     return dict(app = app,db = db,User = User,Pitches = Pitches, Comments= Comments)
