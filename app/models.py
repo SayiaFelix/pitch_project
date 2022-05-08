3dcca1020eab8df03139fe3c6cd0d63fb7e9b750
SayiaFelix <sayia.lucas@student.moringaschool.com>
2022-05-08 11:19:37 +0300

    Fixing bugs

--- app/models.py
+++ app/models.py
@@ -1,9 +1,8 @@
-from . import db
 from werkzeug.security import generate_password_hash,check_password_hash
 from flask_login import UserMixin, current_user
 from . import login_manager
 from datetime import datetime
-
+from . import db
 
 @login_manager.user_loader
 def load_user(user_id):
