8fcd1245e398029d878861b056b01960ab94f59b
SayiaFelix <sayia.lucas@student.moringaschool.com>
2022-05-09 15:17:05 +0300

    fixing bugs

--- app/__init__.py
+++ app/__init__.py
@@ -36,11 +36,8 @@ def create_app(config_name):
     login_manager.init_app(app)
     mail.init_app(app)
 
-
     # Registering the blueprint
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
-
-
 
     return app
--- config.py
+++ config.py
@@ -1,10 +1,8 @@
 import os
 
 class Config:
-
     SECRET_KEY = 'f2GDBACdGn5ZGx$A!gC[:+*/b.JS?('
-  
-    # SQLALCHEMY_DATABASE_URI = 'postgres://lybbhvznzgrbxa:bed0b7b6f15b58eeb87996fde36aa4ae79bc0bd2bb1f7a579582334afc7ccca4@ec2-3-217-113-25.compute-1.amazonaws.com:5432/d8so18ov06lcno'
+    SQLALCHEMY_DATABASE_URI = 'postgres://lybbhvznzgrbxa:bed0b7b6f15b58eeb87996fde36aa4ae79bc0bd2bb1f7a579582334afc7ccca4@ec2-3-217-113-25.compute-1.amazonaws.com:5432/d8so18ov06lcno'
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:jaysafu@localhost/pitcheslist'
     UPLOADED_PHOTOS_DEST ='app/static/photos'
 
@@ -21,31 +19,26 @@ class Config:
     def init_app(app):
         pass
 
-
-
 class ProdConfig(Config):
+     uri = os.getenv("DATABASE_URL")  # or other relevant config var
+     if uri.startswith("postgres://"):
+        uri = uri.replace("postgres://", "postgresql://", 1)
 
-    #  uri = os.getenv("DATABASE_URL")  # or other relevant config var
-    #  if uri.startswith("postgres://"):
-    #     uri = uri.replace("postgres://", "postgresql://", 1)
-
-    #     SQLALCHEMY_DATABASE_URI=uri
+        SQLALCHEMY_DATABASE_URI=uri
         # rest of connection code using the connection string `uri`   
  
     #  SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
-    pass
+
 class TestConfig(Config):
-
-#   SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:jaysafu@localhost/pitcheslist_test'
-  pass
-
+      SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:jaysafu@localhost/pitcheslist_test'
+  
 class DevConfig(Config):
     '''
     Development  configuration child class
     Args:
         Config: The parent configuration class with General configuration settings
     '''
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:jaysafu@localhost/pitcheslist'
+    # SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:jaysafu@localhost/pitcheslist'
     DEBUG = True
 
 config_options = {
